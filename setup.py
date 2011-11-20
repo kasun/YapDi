8dc38f64a596d1a1fe85222b18f30a9761037745
Kasun Herath <kasunh01@gmail.com>
2011-11-20 22:08:09 +0530

    Distutils file added

---
+++ setup.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='yapdi',
+    description='Yet another python Daemon implementation',
+    author='Kasun Herath',
+    author_email='kasunh01@gmail.com',
+    py_modules=['yapdi'],
+    )
+
+
